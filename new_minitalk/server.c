b8ab39a0585afd6f7cf85f9bf4f42753a7637da5
Peter Harbst <pharbst@student.42heilbronn.de>
2022-11-23 18:47:50 +0100

    overflow fixed tested with up to 260.000 chars runs with 15200 chars per second

--- new_minitalk/server.c
+++ new_minitalk/server.c
@@ -6,19 +6,28 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:45:06 by pharbst           #+#    #+#             */
-/*   Updated: 2022/11/23 16:57:39 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/11/23 18:46:00 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	print_block(char *block)
+void	print_massage(char **block)
 {
 	int	i;
-
+	int	j;
+	
 	i = 0;
-	while (block[i] && i < 1024)
-		write(1, &block[i++], 1);
+	while (block[i])
+	{
+		j = 0;
+		while (block[i][j] && j < 1024)
+		{
+			ft_printf("%c", block[i][j]);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	sig_handler(int sig, siginfo_t *siginfo, void *context)
@@ -38,7 +47,6 @@ void	sig_handler(int sig, siginfo_t *siginfo, void *context)
 		ft_printf("Connection established with %d\n", pid);
 		flag[pid] = 1;
 		kill(pid, SIGUSR1);
-		// usleep(100);
 	}
 	else if (flag[pid] == 1)
 	{
@@ -47,26 +55,15 @@ void	sig_handler(int sig, siginfo_t *siginfo, void *context)
 		if (i[pid] < 0)
 			kill(pid, SIGUSR2);
 		if (!c[pid][(i[pid] / 8) / 1024])
-			c[pid][(i[pid] / 8) / 1024] = ft_calloc(1024, sizeof(char));
+			c[pid][(i[pid] / 8) / 1024] = ft_calloc(1025, sizeof(char));
 		if (sig == SIGUSR1)
-		{
 			c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024] = (c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024] << 1) + 1;
-			// ft_printf("1");
-		}
 		if (sig == SIGUSR2)
-		{
 			c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024] = (c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024] << 1);
-			// ft_printf("0");
-		}
 		j = 0;
-		// ft_printf("\ni[pid] / 8 = %d && i[pid] %% 8 = %d && c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) %% 1024] = %c\n", i[pid] / 8, i[pid] % 8, c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024]);
 		if (i[pid] / 8 != 0 && i[pid] % 8 == 7 && c[pid][(i[pid] / 8) / 1024][(i[pid] / 8) % 1024] == '\0')
 		{
-			while (c[pid][j])
-			{
-				print_block(c[pid][j++]);
-				// ft_printf("block printed\n");
-			}
+			print_massage(c[pid]);
 			write(1, "\n", 1);
 			j = 0;
 			while (c[pid][j])
