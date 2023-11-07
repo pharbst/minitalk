8f622ec826ef8640498b6444c78d14b5953a14e4
Peter Harbst <pharbst@3-E-4.42heilbronn.de>
2023-11-07 06:10:00 +0100

    Wip:	client has to be double checkt but it should be close to done

--- src/client/new_client.c
+++ src/client/new_client.c
@@ -6,16 +6,12 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 03:32:14 by pharbst           #+#    #+#             */
-/*   Updated: 2023/11/07 05:24:33 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/11/07 06:04:13 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "new_minitalk.h"
 #include "libftio.h"
-
-t_payload*	get_payload(void);
-void		connect(void);
-void		send_controll_bits(void);
 
 int	main(int argc, char **argv)
 {
@@ -42,29 +38,17 @@ int	main(int argc, char **argv)
 	return (0);
 }
 
-
 void	connect(void)
 {
-	unsigned int	i;
 	t_payload		*payload;
 
 	signal(SIGUSR1, &sig_connect);
 	signal(SIGUSR2, &sig_connect);
-	payload = get_payload();
-	payload->busy = true;
-	while (payload->busy)
+	t_payload		*payload;
+	sending_loop(0, 1);
+	if (!payload->answerd)
 	{
-		kill(payload->dest_pid, SIGUSR1);
-		payload->busy = false;
-		i = 0;
-		while (i++ < 1280 && !payload->busy && !payload->connection)
-			usleep(1);
-	}
-	if (payload->connection)
-		ft_printf("Connection established\n");
-	else
-	{
-		ft_putstr_fd("Error: Connection Failed\n", 1);
+		ft_putstr_fd("Error: Server is not responding\n", 1);
 		exit(0);
 	}
 	if (sending(32, payload->message_len))
@@ -106,37 +90,9 @@ int	sending(unsigned int bit_len, unsigned int what)
 	t_payload		*payload;
 
 	payload = get_payload();
-	bit = bit_len - 1;
-	while (bit >= 0 && payload->answerd)
-	{
-		payload->answerd = false;
-		j = 0;
-		while (j++ < 16 && !payload->answerd)
-		{
-			if ((what >> bit) % 2)
-				kill(payload->dest_pid, SIGUSR2);
-			else
-				kill(payload->dest_pid, SIGUSR1);
-			i = 0;
-			while (!payload->answerd && i++ < 1280)
-				usleep(1);
-			if (payload->busy)
-			{
-				usleep(128000);
-				payload->busy = false;
-				payload->answerd = false;
-			}
-		}
-		bit--;
-	}
+	while (--bit_len >= 0 && payload->answerd)
+		sending_loop(what, bit_len);
 	if (!payload->answerd)
-			return (ft_putstr_fd("Error: Server is not responding\n", 1), 1);
+			return (ft_putstr_fd("Error: lost connection to server\n", 1), 1);
 	return (0);
-}
-
-t_payload*	get_payload(void)
-{
-	static t_payload	_payload;
-
-	return (&_payload);
 }
--- src/client/new_client_helper.c
+++ src/client/new_client_helper.c
@@ -1,16 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new_sighandler.c                                   :+:      :+:    :+:   */
+/*   new_client_helper.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 00:08:44 by pharbst           #+#    #+#             */
-/*   Updated: 2023/11/07 05:23:12 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/11/07 06:06:49 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "new_minitalk.h"
+
+void	sending_loop(unsigned int what, int bit)
+{
+	unsigned int	i;
+	unsigned int	j;
+	t_payload		*payload;
+
+	payload = get_payload();
+	i = 0;
+	payload->answerd = false;
+	while (i++ < 16 && !payload->answerd)
+	{
+		payload->busy = true;
+		while (payload->busy)
+		{
+			payload->busy = false;
+			payload->answerd = false;
+			if ((what >> bit) % 2)
+				kill(payload->dest_pid, SIGUSR2);
+			else
+				kill(payload->dest_pid, SIGUSR1);
+			j = 0;
+			while (!payload->answerd && j++ < 1280)
+				usleep(1);
+			if (payload->busy)
+				usleep(128000);
+		}
+	}
+}
+
+t_payload*	get_payload(void)
+{
+	static t_payload	_payload;
+
+	return (&_payload);
+}
 
 void	sig_controll(int sig)
 {
@@ -22,7 +58,7 @@ void	sig_controll(int sig)
 	else if (sig == SIGUSR2)
 	{
 		payload->answerd = true;
-		payload->busy = true
+		payload->busy = true;
 	}
 }
 
@@ -36,10 +72,12 @@ void	sig_connect(int sig)
 		ft_printf("Connection confirmed\n");
 		payload->connection = true;
 		payload->busy = false;
+		payload->answerd = true;
 	}
 	else if (sig == SIGUSR2)
 	{
 		ft_putstr_fd("Server is busy try to connect again in 128ms\n", 1);
 		payload->busy = true;
+		payload->answerd = true;
 	}
 }
