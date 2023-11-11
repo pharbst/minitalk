ecbe07a80442508e8b57e677b51ec3761365762d
Peter Harbst <pharbst@3-B-1.42heilbronn.de>
2023-11-11 04:02:03 +0100

    Add:	added function for shutting down on ctrl + c to prevent memory 	leaks

--- src/server/new_server.c
+++ src/server/new_server.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 00:18:24 by pharbst           #+#    #+#             */
-/*   Updated: 2023/11/11 00:39:13 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/11/11 00:47:09 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 #include "libftio.h"
 
 static void	server_sig_handler(int sig, siginfo_t *info, void *ucontext);
+static void	sig_int(void);
 
 int	main(void)
 {
 	struct sigaction	sa;
+	struct sigaction	sa_int;
 
 	ft_bzero(&sa, sizeof(sa));
 	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = server_sig_handler;
+	sa.sa_sigaction = &server_sig_handler;
+	ft_bzero(&sa_int, sizeof(sa_int));
+	sa_int.sa_sigaction = &sig_int;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+	sigaction(SIGINT, , NULL);
 	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 		check_timeout();
@@ -57,4 +62,23 @@ static void	server_sig_handler(int sig, siginfo_t *info, void *ucontext)
 		return ;
 	}
 	interpreter(&clients[get_index(info->si_pid, pids, false)], sig, false);
+}
+
+static void	sig_int(void)
+{
+	t_client	*clients;
+	int			i;
+
+	clients = get_clients();
+	ft_printf("Shutting down Server\n");
+	i = 0;
+	ft_printf("cleaning up clients\n");
+	while (i < 256)
+	{
+		if (clients[i].message)
+			free(clients[i].message);
+		i++;
+	}
+	ft_printf("Server is down\n");
+	exit(0);
 }
